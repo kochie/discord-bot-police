57efb1cb3ef3b869defcb62f495f7373c9bd3f06
Robert Koch <robert@kochie.io>
2025-03-23 19:47:11 +1100

    commiunist message is lessa nnoying

--- src/directives/commie.go
+++ src/directives/commie.go
@@ -1,12 +1,9 @@
 package directives
 
 import (
+	"github.com/bwmarrin/discordgo"
 	"github.com/kochie/discord-bot-police/src/util"
 	"log"
-	"os"
-	"path/filepath"
-
-	"github.com/bwmarrin/discordgo"
 )
 
 func CommieDetection(processedString string, s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -20,23 +17,25 @@ func CommieDetection(processedString string, s *discordgo.Session, m *discordgo.
 		"stalin",
 		"kermitthefrog",
 	}) {
-		filename := "assets/Joseph_McCarthy.jpg"
+		//filename := "assets/Joseph_McCarthy.jpg"
 
-		if m.Author.ID == os.Getenv("KNOWN_COMMUNIST_ID") {
-			filename = "assets/commie.mp4"
-		}
+		err := s.MessageReactionAdd(m.ChannelID, m.ID, "ussr:695565659303706745")
 
-		file, err := os.Open(filename)
-		if err != nil {
-			log.Println(err)
-		}
-		_, err = s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{
-			Files: []*discordgo.File{{
-				Name:        filepath.Base(filename),
-				ContentType: util.GetContentType(filename),
-				Reader:      file,
-			}},
-		})
+		//if m.Author.ID == os.Getenv("KNOWN_COMMUNIST_ID") {
+		//	filename = "assets/commie.mp4"
+		//}
+		//
+		//file, err := os.Open(filename)
+		//if err != nil {
+		//	log.Println(err)
+		//}
+		//_, err = s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{
+		//	Files: []*discordgo.File{{
+		//		Name:        filepath.Base(filename),
+		//		ContentType: util.GetContentType(filename),
+		//		Reader:      file,
+		//	}},
+		//})
 		if err != nil {
 			log.Println(err)
 		}
